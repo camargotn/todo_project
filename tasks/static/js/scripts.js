5e2b25c00401cedefd05d221764fb73967f7ee2f
camargotn <thiagocamargo@alumni.usp.br>
2022-11-05 16:42:55 -0300

    delete validation js failed

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,6 +1,20 @@
+console.log('funcionou!');
 
 $(document).ready(function(){
   
   console.log('funcionou!');
-  
-})
+
+  var deleteBtn = $('.detele-btn');
+
+  $(deleteBtn).on('click', function(e) {
+   e.preventDefault();
+   
+   var delLink = $(this).attr('href');
+   var result = confirm('Quer deletar essa tarefa?');
+
+   if(result) {
+    window.location.href = delLink;
+   }
+  });
+
+});
