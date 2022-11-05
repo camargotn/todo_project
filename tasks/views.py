5a5ab819f025dcc60d1c35667e4133d0d199721b
camargotn <thiagocamargo@alumni.usp.br>
2022-11-05 15:54:48 -0300

    edit function

--- tasks/views.py
+++ tasks/views.py
@@ -36,5 +36,18 @@ def editTask(request, id):
   task = get_object_or_404(Task, pk=id)
   form = TaskForm(instance=task)
 
+  if(request.method == 'POST'):
+    form = TaskForm(request.POST, instance=task)
+
+    if(form.is_valid()):
+      task.save()
+      return redirect('/')
+
+    else:
+      return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+  else:
+    return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
 def yourName(request, name):
   return render(request, 'tasks/yourname.html', {'name': name})
